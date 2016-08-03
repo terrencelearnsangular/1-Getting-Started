ff8cb405c7d15cb3ce46dd4d90920901aeb43974
Terrence Persaud Jr <terrence.persaud@gmail.com>
2016-08-03 15:57:09 -0500

    separate http and add logging
    1. Separate http calls
    2. Add logging functionality

--- script.js
+++ script.js
@@ -2,34 +2,36 @@
 
 	var app = angular.module("firstTutorial", [] );
 
-	var MainController = function($scope, $http){
+	var MainController = function($scope, $log, github){
 
-		var onUserComplete = function(response){
-			$scope.user = response.data;
+		var onUserComplete = function(data){
+			$scope.user = data;
 			$scope.userError = null;
-
-			$http.get($scope.user.repos_url)
-				.then(onReposComplete, onReposError);
+			$log.info("User Found");
+			github.getRepos($scope.user).then(onReposComplete, onReposError);
 		};
 
 		var onUserError = function(reason){
 			$scope.userError = reason.statusText || "Request Failed";
 			$scope.user = null;
+			$log.info("User Error: " + $scope.userError);			
 		};
 
-		var onReposComplete = function(response){
-			$scope.repos = response.data;
+		var onReposComplete = function(data){
+			$scope.repos = data;
 			$scope.reposError = null;
+			$log.info("User Repositories Found");			
 		};
 
 		var onReposError = function(reason){
 			$scope.reposError = reason.statusText || "Request Failed";
 			$scope.repos = null;
+			$log.info("Repository Error: " + $scope.reposError);			
 		};
 
 		$scope.search = function(username){
-			$http.get("https://api.github.com/users/" + username)
-				.then(onUserComplete, onUserError);
+			$log.info("Search: " + username);			
+			github.getUser(username).then(onUserComplete, onUserError);
 		};
 
 		$scope.username = "tjworkaccount";
